3b423e30adb8486cd7eb2b686b736f24ff82e31c
Thibaut Mottet <thibaut.mottet@gmai.com>
2019-09-20 18:21:30 +0200

    Add view

--- Sources/SlideOverCard/SlideOverCard.swift
+++ Sources/SlideOverCard/SlideOverCard.swift
@@ -1,3 +1,107 @@
-struct SlideOverCard {
-    var text = "Hello, World!"
+import SwiftUI
+
+struct SlideOverCard<Content: View> : View {
+    @GestureState private var dragState = DragState.inactive
+    @State var position = CardPosition.middle
+    @Environment(\.colorScheme) var colorScheme: ColorScheme
+    
+    var content: () -> Content
+    var body: some View {
+        ZStack {
+            BlurView(style: colorScheme == .dark ? .dark : .extraLight)
+            VStack {
+                Handle()
+                self.content()
+            }
+        }
+        .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+        .cornerRadius(10.0)
+        .shadow(color: Color(.sRGBLinear, white: 0, opacity: 0.13), radius: 10.0)
+        .offset(y: self.position.rawValue + self.dragState.translation.height)
+        .animation(self.dragState.isDragging ? nil : .spring())
+        .gesture(DragGesture().updating($dragState) { drag, state, transaction in
+                state = .dragging(translation: drag.translation)
+            }
+            .onEnded(onDragEnded))
+    }
+    
+    private func onDragEnded(drag: DragGesture.Value) {
+        let verticalDirection = drag.predictedEndLocation.y - drag.location.y
+        let cardTopEdgeLocation = self.position.rawValue + drag.translation.height
+        let positionAbove: CardPosition
+        let positionBelow: CardPosition
+        let closestPosition: CardPosition
+        
+        if cardTopEdgeLocation <= CardPosition.middle.rawValue {
+            positionAbove = .top
+            positionBelow = .middle
+        } else {
+            positionAbove = .middle
+            positionBelow = .bottom
+        }
+        
+        if (cardTopEdgeLocation - positionAbove.rawValue) < (positionBelow.rawValue - cardTopEdgeLocation) {
+            closestPosition = positionAbove
+        } else {
+            closestPosition = positionBelow
+        }
+        
+        if verticalDirection > 0 {
+            self.position = positionBelow
+        } else if verticalDirection < 0 {
+            self.position = positionAbove
+        } else {
+            self.position = closestPosition
+        }
+    }
+}
+
+struct BlurView: UIViewRepresentable {
+    let style: UIBlurEffect.Style
+
+    func makeUIView(context: UIViewRepresentableContext<BlurView>) -> UIView {
+        let view = UIView(frame: .zero)
+        view.backgroundColor = .clear
+        let blurEffect = UIBlurEffect(style: style)
+        let blurView = UIVisualEffectView(effect: blurEffect)
+        blurView.translatesAutoresizingMaskIntoConstraints = false
+        view.insertSubview(blurView, at: 0)
+        NSLayoutConstraint.activate([
+            blurView.heightAnchor.constraint(equalTo: view.heightAnchor),
+            blurView.widthAnchor.constraint(equalTo: view.widthAnchor),
+        ])
+        return view
+    }
+
+    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<BlurView>) {}
+
+}
+
+enum CardPosition: CGFloat {
+    case top = 100
+    case middle = 500
+    case bottom = 850
+}
+
+enum DragState {
+    case inactive
+    case dragging(translation: CGSize)
+    
+    var translation: CGSize {
+        switch self {
+        case .inactive:
+            return .zero
+        case .dragging(let translation):
+            return translation
+        }
+    }
+    
+    var isDragging: Bool {
+        switch self {
+        case .inactive:
+            return false
+        case .dragging:
+            return true
+        }
+    }
 }
